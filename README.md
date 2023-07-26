f113f8177334b22fd4325dd8f358580b837495b9
LAN\OKostyrnaya <OKostyrnaya@lanit.ru>
2023-07-26 18:44:09 +0300

    Valid Palindrome optimized solution

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # java_code_interview
 algorithms training
+
+
+https://leetcode.com/problems/move-zeroes/ DONE
+
+https://leetcode.com/problems/valid-palindrome/ - DONE (неясна сложность алгоритма, неупрощенного)
+
+https://leetcode.com/problems/string-compression/ DONE
+
+https://leetcode.com/problems/summary-ranges/ DONE (неоптимальное решение)
+
+https://leetcode.com/problems/two-sum/ DONE
+
+
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+https://leetcode.com/problems/squares-of-a-sorted-array
+
+https://leetcode.com/problems/add-two-numbers/
+
+https://leetcode.com/problems/reverse-linked-list/
+
+https://leetcode.com/problems/insert-delete-getrandom-o1
--- leetcode/validpalindrome/ValidPalindrome.java
+++ leetcode/validpalindrome/ValidPalindrome.java
@@ -26,4 +26,30 @@ public class ValidPalindrome {
     public String removeNonAlphaNumericSymbols(String s) {
         return s.replaceAll("[^a-zA-Z0-9]", "");
     }
+
+    public boolean isPalindromeOptimized(String s) {
+        if (s == null || s.isEmpty()) {
+            return true;
+        }
+
+        int tail = s.length() - 1;
+        int head = 0;
+        while (head <= tail) {
+            char chHead = s.charAt(head);
+            char chTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(chHead)) {
+                head++;
+            } else if (!Character.isLetterOrDigit(chTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(chHead) != Character.toLowerCase(chTail)) {
+                    return false;
+                }
+                tail--;
+                head++;
+            }
+        }
+
+        return true;
+    }
 }
