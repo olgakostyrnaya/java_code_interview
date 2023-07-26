0e9b95b6b0d5622b99726f0f0ab0afa532bcdd48
LAN\OKostyrnaya <OKostyrnaya@lanit.ru>
2023-07-26 16:00:40 +0300

    SummaryRanger minor changes

--- leetcode/summary-ranges/SummaryRanger.java
+++ leetcode/summary-ranges/SummaryRanger.java
@@ -2,23 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-//class Solution {
-//    public List<String> summaryRanges(int[] nums) {
-//        List<String> ans = new ArrayList<>();
-//        for (int i = 0; i < nums.length; ++i) {
-//            final int begin = nums[i];
-//            while (i + 1 < nums.length && nums[i] == nums[i + 1] - 1)
-//                ++i;
-//            final int end = nums[i];
-//            if (begin == end)
-//                ans.add("" + begin);
-//            else
-//                ans.add("" + begin + "->" + end);
-//        }
-//        return ans;
-//    }
-//}
-
 public class SummaryRanger {
     public List<String> summaryRanges(int[] nums) {
         if (nums == null) {
@@ -59,4 +42,20 @@ public class SummaryRanger {
 
         return result;
     }
+
+//        Эталонное решение c leetcode
+//        public List<String> summaryRanges(int[] nums) {
+//        List<String> ans = new ArrayList<>();
+//        for (int i = 0; i < nums.length; ++i) {
+//            final int begin = nums[i];
+//            while (i + 1 < nums.length && nums[i] == nums[i + 1] - 1)
+//                ++i;
+//            final int end = nums[i];
+//            if (begin == end)
+//                ans.add("" + begin);
+//            else
+//                ans.add("" + begin + "->" + end);
+//        }
+//        return ans;
+//    }
 }
